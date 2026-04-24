4dea9c782e8ecdd128a1e2e3fd09597e63bc61aa
vsalom505-dot <vsalom505@gmail.com>
2026-04-24 12:01:00 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const { notifyUpdate } = require('./utils/api');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("DB Connected"));
+  .then(() => console.log("DB Connected"))
+  .catch(err => console.error("DB Error:", err));
 
 client.once('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
@@ -39,12 +40,16 @@ client.on('interactionCreate', async (interaction) => {
   if (interaction.isButton()) {
     const t = await Tournament.findOne();
 
+    if (!t || !t.matches?.length) {
+      return interaction.reply("No tournament or match found.");
+    }
+
     t.matches[0].winner = "Team A";
     await t.save();
 
     await notifyUpdate(t.name);
 
-    interaction.reply("Match updated!");
+    return interaction.reply("Match updated!");
   }
 
 });
